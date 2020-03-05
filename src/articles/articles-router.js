cf2cc9c399d332cb3c0c109c36b7346cad3d0699
Ben Hernandez <benjammin234@gmail.com>
2020-03-05 14:19:18 -0800

    Adds author to articles router

--- src/articles/articles-router.js
+++ src/articles/articles-router.js
@@ -11,7 +11,8 @@ const serializeArticle = article => ({
   style: article.style,
   title: xss(article.title),
   content: xss(article.content),
-  date_published: article.date_published
+  date_published: article.date_published,
+  author: article.author
 });
 
 articlesRouter
@@ -25,7 +26,7 @@ articlesRouter
       .catch(next);
   })
   .post(jsonParser, (req, res, next) => {
-    const { title, content, style } = req.body;
+    const { title, content, style, author } = req.body;
     const newArticle = { title, content, style };
 
     for (const [key, value] of Object.entries(newArticle)) {
@@ -35,6 +36,8 @@ articlesRouter
           .json({ error: { message: `Missing '${key}' in request body` } });
       }
     }
+
+    newArticle.author = author;
 
     ArticlesService.insertArticle(req.app.get("db"), newArticle)
       .then(article => {
@@ -78,13 +81,11 @@ articlesRouter
     const numberOfValues = Object.values(articleToUpdate).filter(Boolean)
       .length;
     if (numberOfValues === 0) {
-      return res
-        .status(400)
-        .json({
-          error: {
-            message: `Request body must contain either 'title', 'style', or 'content'`
-          }
-        });
+      return res.status(400).json({
+        error: {
+          message: `Request body must contain either 'title', 'style', or 'content'`
+        }
+      });
     }
 
     ArticlesService.updateArticle(
