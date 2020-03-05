764fb6295a3224c6658da2745848656056372e69
Ben Hernandez <benjammin234@gmail.com>
2020-03-05 13:52:37 -0800

    Fixes delete

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -24,7 +24,7 @@ const UsersService = {
   deleteUser(knex, id) {
     return knex("blogful_users")
       .where({ id })
-      .first();
+      .delete();
   },
 
   updateUser(knex, id, newUserFields) {
